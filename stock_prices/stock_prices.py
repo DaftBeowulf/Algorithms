080ee60e07f48a6b2b382e8ad8b758066b98bea0
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-17 15:27:01 -0500

    :sparkles: Stock prices complete
    stock prices written and tests ok

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,15 +1,59 @@
 #!/usr/bin/python
 
 import argparse
+import math
+"""
+find_max_profit([10, 7, 5, 8, 11, 9]) = 6
+return 11-5 =6
+
+for each possible buy number(each except the last),
+find highest profit difference in array items AFTER it (sell) 
+[10, 7, 5, 8, 11, 9]
+  b            s
+p=1
+
+[10, 7, 5, 8, 11, 9]
+     b         s
+p=4
+
+[10, 7, 5, 8, 11, 9]
+        b      s
+p=6
+
+[10, 7, 5, 8, 11, 9]
+           b  s
+p is still 6
+
+[10, 7, 5, 8, 11, 9]
+               b  s
+p is still 6
+
+loop ends, return 6  
+"""
+
 
 def find_max_profit(prices):
-  pass
 
+    # declare lowest number possible to catch inputs with only negative profits
+    profit = -math.inf
+
+    for i, buy in enumerate(prices[:-1]):
+        for sell in prices[i+1:]:
+            if sell-buy > profit:
+                profit = sell-buy
+
+    return profit
+
+
+print(find_max_profit([10, 7, 5, 8, 11, 9]))
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
