8b8b116cd426dd4b252a0167916c83704219843b
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-18 10:37:50 -0500

    :construction: First pass at making change not working
    Adapted eating cookies algorithm to making_change, but key difference has come up: e_c was concerned with the totaly number of ways the cookies could be eaten, including the ORDER in which certain amounts were eaten, ex: eating 1 then 2 cookies is a different result than eating 2 then 2. making_change does not care about the order in which the change is subtracted

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,39 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+denominations = [1, 5, 10, 25, 50]
 
+
+def making_change(amount, denominations, cache=[]):
+    if cache == []:
+        cache = [0 for i in range(amount+1)]
+        denominations.reverse()
+    if amount < 0:
+        return 0
+    if 0 <= amount <= 1:
+        return 1
+    if cache[amount] > 0:
+        return cache[amount]
+    else:
+        for coin in denominations:
+            if amount-coin > 0:
+                cache[amount] += making_change(amount -
+                                               coin, denominations, cache)
+            elif amount-coin == 0:
+                cache[amount] += 1
+
+    return cache[amount]
+
+
+print(making_change(10, [1, 5, 10, 25, 50]))
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
